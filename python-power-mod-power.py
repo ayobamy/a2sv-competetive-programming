132aefebbc11e6f99d136f01c26b1cdd5464c7a5
ayobamy <ayobamyahmed@gmail.com>
2024-02-09 23:11:09 +0100

    (feat): python-power and mod power

---
+++ python-power-mod-power.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    
+    print(a ** b)
+    print((a ** b) % c)
+    
